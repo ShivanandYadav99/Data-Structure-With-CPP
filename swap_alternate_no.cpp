b63a38d58edfe14e62845eb0e723ac6f139ad587
Shivanand Yadav <111229426+ShivanandYadav99@users.noreply.github.com>
2022-12-02 00:31:57 +0530

    P4

---
+++ swap_alternate_no.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void alternate(int arr[],int n){
+    int i=0;
+    while(i<n){
+        if(i+1<n){
+            swap(arr[i],arr[i+1]);
+            
+            }
+            i=i+2;
+    }
+}
+int main(){
+    int arr[100],n;
+    cout<<"enter the size of array"<<endl;
+    cin>>n;
+    cout<<"enter the array element"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    alternate(arr,n);
+    cout<<"after swapping value "<<endl;
+    for(int i=0;i<n;i++){
+    
+    cout<<arr[i]<<endl;
+    }
+
+}
