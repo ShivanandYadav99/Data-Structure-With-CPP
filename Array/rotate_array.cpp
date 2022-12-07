5f1705dd953de58aa9efaa99e49e13a7ec409eab
Shivanand Yadav <111229426+ShivanandYadav99@users.noreply.github.com>
2022-12-08 00:04:01 +0530

    Add files via upload
    P12

---
+++ Array/rotate_array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    
+
+    vector<int>v;
+    v.push_back(1);
+    v.push_back(12);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    int k=2;
+    k=k%v.size();
+
+    reverse(v.begin(),v.end());
+    reverse(v.begin()+k,v.end());
+    reverse(v.begin()+k,v.end());
+
+    for(int a:v){
+        cout<<a<<" ";
+    }
+}
