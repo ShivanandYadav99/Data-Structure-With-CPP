974650169400131716fc834271b119b11686af6e
Shivanand Yadav <111229426+ShivanandYadav99@users.noreply.github.com>
2022-12-14 22:05:04 +0530

    P23

---
+++ Array/move_all_negative_element_one_side.cpp
@@ -0,0 +1,33 @@
+// move all negative number in oneside
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void rearrange(int arr[], int n)
+{
+	int j = 0;
+	for (int i = 0; i < n; i++) {
+		if (arr[i] < 0) {
+			if (i != j)
+				swap(arr[i], arr[j]);
+			j++;
+		}
+	}
+}
+
+
+void printArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+}
+
+
+int main()
+{
+	int arr[] = { -1,-3,4,3,-5,-7,1,2,3 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	rearrange(arr, n);
+	printArray(arr, n);
+	return 0;
+}
